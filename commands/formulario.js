db893011f787e5aaebba4acea58e64cc5abd3dad
rwedfdddfsdfsdwer <42014365+rwedfdddfsdfsdwer@users.noreply.github.com>
2018-08-26 02:20:32 -0300

    Update formulario.js

--- commands/formulario.js
+++ commands/formulario.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-    if(message.channel.id != "461944635808284683") return message.reply(`você não pode executar comandos aqui, vá em <#461944635808284683>`).then(msg => msg.delete(5000))
+    if(message.channel.id != "482929569033093121") return message.reply(`você não pode executar comandos aqui, vá em <#482929569033093121>`).then(msg => msg.delete(5000))
    message.delete().catch(O_o=>{})
     const form = new Discord.RichEmbed()
        .setColor("#4a4a4a")
